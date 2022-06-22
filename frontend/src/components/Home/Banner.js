6c642eaa70a29f47790d2523fd9393cc86abd7c6
Aaron Osher <aaron@aaronosher.io>
2022-06-22 23:35:29 +0000

    Add search box

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import agent from "../../agent";
+import { APPLY_TITLE_FILTER } from "../../constants/actionTypes";
 import logo from "../../imgs/logo.png";
 
-const Banner = () => {
+const mapStateToProps = (state) => ({
+  ...state.home,
+  appName: state.common.appName,
+  token: state.common.token,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onSearch: (titleQuery, pager, payload) =>
+    dispatch({ type: APPLY_TITLE_FILTER, titleQuery, pager, payload }),
+});
+
+const Banner = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    if (query.length > 2) {
+      onSearch(
+        query,
+        (page) => agent.Items.all(page, query),
+        agent.Items.all(0, query)
+      )
+    } else {
+      onSearch(
+        query,
+        (page) => agent.Items.all(page),
+        agent.Items.all(0)
+      )
+    }
+  }, [query, onSearch]);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div>
+        <div className="d-flex justify-content-center form-inline mx-5">
           <span id="get-part">A place to get</span>
+          <div className="input-group flex-grow-1 mx-2">
+            <input 
+              type="text"
+              placeholder="What is it that you truly desire?"
+              id="search-box" 
+              className="form-control form-control-lg"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)} />
+          </div>
           <span> the cool stuff.</span>
         </div>
       </div>
@@ -15,4 +56,4 @@ const Banner = () => {
   );
 };
 
-export default Banner;
+export default connect(mapStateToProps, mapDispatchToProps)(Banner);
